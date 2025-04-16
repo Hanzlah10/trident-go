3026ac81cb5edb6537772d83de45c8a97c7ec0fd
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-04-16 16:32:42 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,46 @@ module.exports = {
         chakra: ['"Chakra Petch"', 'sans-serif'],
         opensans: ['"Open Sans"', 'sans-serif'],
         poppins: ['"Poppins"', 'sans-serif'],
+      },
+      colors: {
+        'gold': '#EAC96F'
       }
     },
     backgroundImage: {
-      'custom-fade': 'linear-gradient(to right, rgba(78, 78, 84, 0) 0%, #ADADBA 45%, rgba(78, 78, 84, 0) 99%)',
+      'custom-fade': `linear-gradient(
+                      to right,
+                      rgba(78, 78, 84, 0) 0%, 
+                      #ADADBA 45%, 
+                      rgba(78, 78, 84, 0) 99%
+                      )`,
+
+      'gold-gradient': `linear-gradient(
+                        90deg,
+                        #DBB346 0%,
+                        #FFE7A7 36%,
+                        #FFCF4B 57%,
+                        #FFF4D1 77%,
+                        #E4B73D 100%
+                      )`,
+      'mint-gradient': `linear-gradient(
+          to bottom,
+          #E8FFE0 0%,
+          #EEFFC9 46%,
+          #77A587 100%
+        )`,
+      'mint-gradient-t': `linear-gradient(
+          to top,
+          #E8FFE0 0%,
+          #EEFFC9 46%,
+          #77A587 100%
+        )`,
+      'pink-purple-gradient': `linear-gradient(
+          to bottom,
+          #FF89DF 0%,
+          #2C0F25 100%
+        )`,
+      'lime-gradient': 'linear-gradient(to bottom, #F8FFBA 0%, #ECFF4F 42%, #D0E050 100%)',
+      'emerald-dark': 'linear-gradient(90deg, #152B29 0%, #134C3C 47%, #152B29 100%)',
     }
   },
   plugins: [],
