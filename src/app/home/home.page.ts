1f9d2e465f83a445fbf0f49376898c7276953dee
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-04-16 21:00:52 +0530

    feat: implement quiz navigation and enhance question component layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, NavbarComponent],
+  imports: [IonContent, NavbarComponent, RouterLink],
 
 })
 export class HomePage {
--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+import { QuestionComponent } from 'src/app/components/question/question.component';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.page.html',
   styleUrls: ['./quiz.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, QuestionComponent, NavbarComponent]
 })
 export class QuizPage implements OnInit {
 
