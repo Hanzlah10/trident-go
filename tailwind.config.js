a6b750aa8cf83bfa34bdfcf2bf2656a4e3d562e4
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-04-16 11:04:05 +0530

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        vietnam: ['"Be Vietnam Pro"', 'sans-serif'],
+        chakra: ['"Chakra Petch"', 'sans-serif'],
+        opensans: ['"Open Sans"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+      }
+    },
+    backgroundImage: {
+      'custom-fade': 'linear-gradient(to right, rgba(78, 78, 84, 0) 0%, #ADADBA 45%, rgba(78, 78, 84, 0) 99%)',
+    }
   },
   plugins: [],
 };
