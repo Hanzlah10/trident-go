f3fd6b77f6a67f8d1035436ae47dcb78143e84e7
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-04-17 21:26:55 +0530

    feat: integrate navbar component and enhance layout for leaderboard and quiz pages

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonButtons, IonBackButton, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  imports: [IonButtons, IonBackButton]
 })
-export class NavbarComponent  implements OnInit {
+export class NavbarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
--- src/app/pages/leaderboard/leaderboard.page.ts
+++ src/app/pages/leaderboard/leaderboard.page.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.page.html',
   styleUrls: ['./leaderboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonContent, NavbarComponent]
 })
 export class LeaderboardPage implements OnInit {
 
